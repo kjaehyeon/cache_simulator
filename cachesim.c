ef4d3d4b9aaeed5fbc16e90872f159b75bce047a
kjaehyeon <jacob9404@gmail.com>
2020-12-02 23:38:03 +0900

    메모리 읽기, victim찾기

--- cachesim.c
+++ cachesim.c
@@ -4,16 +4,22 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+int totalhit = 0;
+int totalmiss = 0;
+int time_count = 0;
 typedef struct _cacheEnt{
 	bool valbit;
 	bool dirbit;
 	int tag;
+	int time;
 	int* data;
 }cacheEnt;
 
+typedef struct _memoryEnt* memoryPointer;
 typedef struct _memoryEnt{
-	int address;
-	int data;
+	unsigned int address;
+	int *data;
+	memoryPointer link;
 }memoryEnt;
 typedef struct _parsedAdd{
 	int byteoff;
@@ -21,9 +27,11 @@ typedef struct _parsedAdd{
 	unsigned int index;
 	unsigned int tag;
 }parsedAdd;
+
 cacheEnt **cache;
-memoryEnt *memory;
-int csiz, bsiz, snum;
+memoryPointer memory = NULL;
+
+int csiz, bsiz, snum;//csiz : cache size, bsiz : block size, snum : # of set
 char* adjarg(char str[]){
 	char* tmpstr;
 	tmpstr = str +1;	
@@ -37,7 +45,19 @@ int log2(int num){
 	}
 	return count;	
 }
-parsedAdd parsingAdd(char addr[]){
+int get_victim(parsedAdd addinfo){//find victim set number for spill
+	int min = cache[0][addinfo.index].time;
+	int setnum = 0;
+
+	for(int i = 1; i < snum; i++){
+		if(cache[i][addinfo.index].time < min){
+			setnum = i;
+			min = cache[i][addinfo.index].time;
+		}
+	}
+	return setnum;
+}
+parsedAdd parsingAdd(char addr[]){//parsing string type address and get info
 	unsigned int address = strtoul(addr, NULL,16);
 	unsigned int mask = 1;
 	parsedAdd tmp;
@@ -46,21 +66,24 @@ parsedAdd parsingAdd(char addr[]){
 	int idxdigit = log2((csiz/snum)/bsiz);
 	int tagdigit = 32 - bytedigit - blockdigit - idxdigit;
 	printf("%d %d %d %d\n",bytedigit, blockdigit, idxdigit,tagdigit);	
+	tmp.byteoff = 0;
 	for(int i = 0; i < bytedigit; i++){
 		tmp.byteoff += address & mask;
 		mask = mask << 1;
 	}
+	tmp.blockoff = 0;
 	for(int i = 0 ; i < blockdigit; i++){
 		tmp.blockoff += address & mask;
 		mask = mask << 1;
 	}
 	tmp.blockoff = tmp.blockoff >> bytedigit;
+	tmp.index = 0;
 	for(int i = 0 ; i < idxdigit; i++){
 		tmp.index += address & mask;
 		mask = mask << 1;
-		printf("%d %d %d\n",address, mask, tmp.index);
 	}
 	tmp.index = tmp.index >> (bytedigit + blockdigit);
+	tmp.tag = 0;
 	for(int i = 0; i < tagdigit; i++){
 		tmp.tag += address & mask;
 		mask = mask << 1;
@@ -82,7 +105,7 @@ void creatCache(){
 void printCache(cacheEnt **cache){
 	int ssiz = csiz/snum;
 	int idxnum = ssiz/bsiz;
-
+	
 	for(int i = 0; i < idxnum; i++){
 		printf("%d: ",i);
 		for(int j = 0; j < snum; j++){
@@ -93,16 +116,131 @@ void printCache(cacheEnt **cache){
 		}
 	}
 }
-void mem_read(char addr[]){
-	int address = strtoul(addr,NULL,16);
+void mem_read(char addr[], int setnum, parsedAdd addinfo){
+	memoryPointer tmp, ptr;
+	unsigned int iaddr = strtoul(addr, NULL, 16);
 	
-}
-void cache_write(char addr[], int data){
-	//byteoffset, index, tag
-	
-	
-	int address = strtoul(addr,NULL,16);
+	iaddr = iaddr >> (2+log2(bsiz/4));//cut byteoffset and blockoffset bit
+	iaddr = iaddr << (2+log2(bsiz/4));	
+	if(memory == NULL){//if memory is empty
+		memory = (memoryPointer)malloc(sizeof(memoryEnt));
+		memory->data = (int*)calloc(bsiz/4, sizeof(int));
+		memory->address = iaddr;
+		memory->link = NULL;
 
+		memcpy(cache[setnum][addinfo.index].data, memory->data, sizeof(int)*(bsiz/4));
+		cache[setnum][addinfo.index].valbit = true;
+		cache[setnum][addinfo.index].dirbit = false;
+		cache[setnum][addinfo.index].tag = addinfo.tag;
+		cache[setnum][addinfo.index].time = time_count++;
+	}
+	else{
+		ptr = memory;
+		while( ((ptr->address) >> (2+log2(bsiz/4)))  != iaddr >>  (2+log2(bsiz/4)) ){
+			ptr = ptr->link;
+			if(ptr == NULL) break;
+		}
+
+		if(ptr != NULL){//if there is data at this address in memory
+			memcpy(cache[setnum][addinfo.index].data,
+				       	ptr->data, sizeof(int)*(bsiz/4));
+			cache[setnum][addinfo.index].valbit = true;
+			cache[setnum][addinfo.index].dirbit = false;
+			cache[setnum][addinfo.index].tag = addinfo.tag;
+			cache[setnum][addinfo.index].time = time_count++;
+		}else{
+			ptr = memory;
+			while(ptr->link != NULL)
+				ptr = ptr->link;
+
+			tmp = (memoryPointer)calloc(1,sizeof(*tmp));
+			tmp->address = iaddr; 
+			tmp->data = (int*)calloc(bsiz/4, sizeof(int));
+			tmp->link = NULL;
+			memcpy(cache[setnum][addinfo.index].data,
+				       	tmp->data, sizeof(int)*(bsiz/4));
+			cache[setnum][addinfo.index].valbit = true;
+			cache[setnum][addinfo.index].dirbit = false;
+			cache[setnum][addinfo.index].tag = addinfo.tag;
+			cache[setnum][addinfo.index].time = time_count++;
+
+			ptr->link = tmp;
+
+		}
+	}
+		
+}
+void mem_write(char addr[], int setnum, parsedAdd addinfo){
+	//kick out the victim (cache to memory)
+	memoryPointer ptr = memory;
+	unsigned int iaddr = strtoul(addr, NULL, 16);
+	
+	iaddr = iaddr >> (2+log2(bsiz/4));//cut byteoffset and blockoffset bit
+	iaddr = iaddr << (2+log2(bsiz/4));
+	
+	//while(ptr->
+
+}
+int cache_read(char addr[]){//read from cache
+	parsedAdd addinfo = parsingAdd(addr);
+	int setnum = 0;
+	while(setnum < snum){//findout whether hit or miss
+		if(cache[setnum][addinfo.index].tag == addinfo.tag
+				&& cache[setnum][addinfo.index].valbit == true)
+			break;
+		setnum++;
+	}
+	
+	if(setnum < snum){//hit!
+		totalhit++;
+		cache[setnum][addinfo.index].time = time_count++;	
+		return setnum;
+	}else{//miss..
+		totalmiss++;
+		setnum = 0;
+		while(cache[setnum][addinfo.index].valbit != false){
+			setnum++;
+			if(setnum >= snum)
+				break;
+		}
+		if(setnum < snum){//if find empty space
+			mem_read(addr, setnum, addinfo);
+			return setnum;
+		}
+		else{//need to spill
+			printf("spill\n");
+			setnum = get_victim(addinfo);
+			if(cache[setnum][addinfo.index].dirbit == true)
+				mem_write(addr,setnum,addinfo);
+			mem_read(addr, setnum, addinfo);
+			return setnum;
+		}
+	}
+
+}
+void cache_write(char addr[], int data){//write to cache
+	parsedAdd addinfo = parsingAdd(addr);
+	int setnum = 0;
+	
+	setnum = cache_read(addr);//get set number to write	
+
+	cache[setnum][addinfo.index].data[addinfo.blockoff] &= ~(1 << (addinfo.byteoff *2));
+	cache[setnum][addinfo.index].data[addinfo.blockoff] &= ~(1 <<(addinfo.byteoff *2 +1));
+
+	cache[setnum][addinfo.index].data[addinfo.blockoff] += (data << (addinfo.byteoff *2*4));
+	printf("write data : %08X\n",cache[setnum][addinfo.index].data[addinfo.blockoff]);
+	cache[setnum][addinfo.index].dirbit = true;
+	cache[setnum][addinfo.index].time = time_count++;
+
+}
+void print_mem(){
+	memoryPointer ptr;
+	ptr = memory;
+	printf("<memory>\n");
+	while(ptr != NULL){
+		printf("%08X %08X %08X\n",ptr->address, ptr->data[0], ptr->data[1]);
+		ptr = ptr->link;
+	}
 }
 int main(int argc, char* argv[]){
 	//cache[set_number][index_number]
@@ -112,7 +250,7 @@ int main(int argc, char* argv[]){
 	char opt;
 	extern char *optarg;
 	FILE *fp;
-
+	
 	while((opt =getopt(argc, argv, "s:b:a:f:")) != -1){
 		switch(opt){
 			case 's':
@@ -131,21 +269,24 @@ int main(int argc, char* argv[]){
 	}
 	printf("s:%d b:%d a:%d f:%s\n",csiz,bsiz,snum,filename);
 	creatCache();
-	parsedAdd tmp = parsingAdd("00010027");
-	printf("%d %d %d %d \n",tmp.byteoff, tmp.blockoff, tmp.index, tmp.tag);
-
-	/*fp = fopen(filename, "r");
+	
+	fp = fopen(filename, "r");
 	while(!feof(fp)){
 		char addr[9];
 		char cmd;
 		int data;
-		fscanf(fp, "%s %c",addr, &cmd);
+		fscanf(fp, "%s %c ",addr, &cmd);
 		if(cmd == 'W'){
-			fscanf(fp,"%d",&data);
+			fscanf(fp,"%d ",&data);
+			if(data > 128)
+				fprintf(stderr,"data is too big\n");
+			printf("%s %c %d\n",addr, cmd, data);
 			cache_write(addr, data);
 		}else if(cmd == 'R'){
-			mem_read(addr);
+			cache_read(addr);
+			printf("%s %c\n",addr, cmd);
 		}
-	}*/
+	}
 	printCache(cache);
+	print_mem();
 }
--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 int log2(int num){
 	int count = 0;
@@ -32,6 +33,16 @@ int main(void){
 	printf("log2 -> 1:%d 2:%d 4:%d 8:%d 16:%d 256:%d\n",log2(1), log2(2), log2(4), log2(8)
 			,log2(16),log2(256));
 	printf("%d %d %d %d\n",2, log2(8/4), log2((64/2)/8), 32-2-log2(8/4)-log2((64/2)/8));
-	printf("%d\n",65575&16);
+	printf("addr : %d ,%8X\n",addr, addr);
+	printf("%d %d\n",addr&16, addr&32);
+
+	int arr[3] = {1,2,3};
+	int *carr = (int*)calloc(3, sizeof(int));
+	memcpy(carr,arr,sizeof(int)*3);
+	printf("%d %d %d\n",carr[0],carr[1],carr[2]);
+	int a = 15;
+	a = a >> 2;
+	a = a << 2;
+	printf("%d\n",a);
 }
 
